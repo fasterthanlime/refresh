3a1027d1474839b44f432ed74941ad82f3d77e35
Amos Wenger <amoswenger@gmail.com>
2023-04-08 20:03:06 +0200

    connect to postgres database

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::postgres::PgPoolOptions;
 use tokio::{process::Command, net::{TcpListener, TcpStream}};
 
 
@@ -9,6 +10,41 @@ async fn main() {
         eprintln!("Violently shutting down");
         std::process::exit(0);
     });
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url).await.unwrap();
+
+    // create the "files" table, indexed by a TEXT column named "path_and_hash"
+    // and with a BYTEA column named "data"
+    sqlx::query(
+        "CREATE TABLE IF NOT EXISTS files (
+            path_and_hash TEXT PRIMARY KEY,
+            data BYTEA NOT NULL
+        )"
+    ).execute(&pool).await.unwrap();
+
+    // create the "revision_files" table indexed by a TEXT column named
+    // "revision", a TEXT column named "path_and_hash"
+    sqlx::query(
+        "CREATE TABLE IF NOT EXISTS revision_files (
+            revision_id TEXT NOT NULL,
+            path_and_hash TEXT NOT NULL,
+            PRIMARY KEY (revision_id, path_and_hash)
+        )"
+    ).execute(&pool).await.unwrap();
+
+    // create the "latest_revision" table indexed by a TEXT column named "latest"
+    // with a "revision_id" TEXT column
+    sqlx::query(
+        "CREATE TABLE IF NOT EXISTS latest_revision (
+            latest TEXT PRIMARY KEY,
+            revision_id TEXT NOT NULL
+        )"
+    ).execute(&pool).await.unwrap();
+
+    println!("All migrations applied");
 
     let mut cmd = Command::new("deno");
     cmd.arg("run").arg("-A").arg("main.ts");
